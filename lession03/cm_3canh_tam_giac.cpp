374c494f96c64b946369db302c6f80f6a75bd7f1
nthuongg <nguyenhuongg1104@gmail.com>
2022-10-17 20:52:25 +0700

    lession03

---
+++ lession03/cm_3canh_tam_giac.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	float a;
+	printf("Pleasse input first number:");
+	scanf ("%f",&a);
+	float b;
+	scanf ("%f",&b);
+	float c;
+	scanf ("%f",&c);
+	if((a+b>c)&&(b+c>a)&&(c+a>b)){
+		float p= (a+b+c)/2;
+		float S= sqrt(p*(p-a)*(p-b)*(p-c));
+		float C= a+b+c;
+		printf ("dien tich tam giac la S=%f va chu vi tam giac la C=%f",S,C);
+	}else{
+		printf ("khong phai la 3 canh cua tam giac");
+	}
+}
